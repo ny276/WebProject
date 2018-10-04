544bc6107fe3bacba4660ae273313e7ecbc4ce85
D7701 <D7701@DESKTOP-ODBNK9S>
2018-10-04 14:58:39 +0900

    컨트롤러 설정 연습

--- exBean/WebContent/read.jsp
+++ exBean/WebContent/read.jsp
@@ -1,12 +1,30 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="java.sql.*" %>
+<%@ page import = "java.util.*" %>
+<%@ page import = "exBean.studentVO" %>
+<%@ page import = "exBean.studentDAO" %>
+
+<%-- DAO 객체 생성 --%>
+<% 
+  studentDAO dao = new studentDAO();
+  List<studentVO> items = new ArrayList<studentVO>();
+  items = dao.selectStudent(); //selectStudent()메서드를 수행하여 List로 record set을 넘겨 받는다
+%>
+
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
 <body>
 
+<%
+ for(int i=0;i<items.size();i++) {
+  studentVO item = (studentVO)items.get(i); //List에서 record를 하나씩 가져온다
+%>
+
+<%=item.getNo()%>
+<%=item.getName()%><br>
+<%}%>
 </body>
 </html>
+
--- exBean/src/exBean/studentDAO.java
+++ exBean/src/exBean/studentDAO.java
@@ -1,5 +1,7 @@
 package exBean;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 public class studentDAO {
  public void insertStudent(studentVO item) throws Exception {
   String no = item.getNo();
@@ -16,4 +18,28 @@ public class studentDAO {
   pstmt.close();
   conn.close();
  }
+ 
+ public List<studentVO> selectStudent() throws Exception { //List에 record set을 담아 넘겨 준다
+	   List<studentVO> items = new ArrayList<studentVO>();
+
+	   Class.forName("oracle.jdbc.driver.OracleDriver");
+	   Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","201730858","201730858"); // DB 접속
+	   
+	   PreparedStatement pstmt = conn.prepareStatement("select * from student");
+	   ResultSet rs = pstmt.executeQuery();// SQL문 실행
+
+	   while(rs.next()) {
+		   studentVO item = new studentVO();
+		   item.setNo(rs.getString("no"));
+		   item.setName(rs.getString("name"));
+		  // 가져온 레코드 값 리스트에 세트
+		   items.add(item);
+	   } 
+
+	   pstmt.close();
+	   rs.close();
+	   conn.close();
+	   return items;
+	 }
 }
+ 
